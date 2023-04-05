47d641b18aff05970dc0501a02bfebe7cf55518b
REJOINT <brecab120@gmail.com>
2023-04-04 21:16:19 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Card className="bg-dark text-white">
+      <Card.Img src="https://www.instacart.com/assets/homepage/app_download_banner2_800-f2d083c6672f81a0e221721b335c8012191af80535bd1310b090fce1baad619f.jpg" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>Card title</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
   );
 }
 
